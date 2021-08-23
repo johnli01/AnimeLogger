54079b425145c7382d90f2eb68a26389617cc521
johnli01 <john.li9631@gmail.com>
2021-08-22 20:02:39 -0700

    Changed approach, uses url to determine current ep

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-alert("Hello")
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', afterDOMLoaded());
+} else {
+  afterDOMLoaded();
+}
+
+function afterDOMLoaded() {
+  var url = document.URL;
+  var ep = url.substring(url.indexOf('ep-'), url.length);
+  alert(ep);
+}
