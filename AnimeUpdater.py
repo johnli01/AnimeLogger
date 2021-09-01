2f8420fcf39d613ee8bd54fa919defd2557d2d9f
johnli01 <john.li9631@gmail.com>
2021-09-01 01:48:57 -0700

    Can now use MAL API, retrieved anime id on MAL

--- AnimeUpdater.py
+++ AnimeUpdater.py
@@ -1,31 +1,53 @@
 from flask import Flask, request
 import requests
 
+# MAL Access key (necessary to make changes and retrieve data on user's MAL list)
+key = {"Authorization": "Bearer ACCESS KEY"}
+
 app = Flask(__name__)
 
+
+# Retrieves the sent anime data from the Google Chrome Extension in a JSON type
 @app.route('/anime_data', methods=['GET', 'POST'])
 def retrieveData():
   with app.app_context():
-    global data
     if request.method == 'POST':
-      print("RUNNING")
+      # Checks to see if the sent JSON data contains all the correct information
       if not request.form['title'] or not request.form['jtitle'] or not request.form['ep']:
         print('Enter all fields properly')
       else:
-        print(request.form['title'])
+        # Saves JSON information into variables
         title = request.form['title']
         jtitle = request.form['jtitle']
         ep = request.form['ep']
 
-        print(request.form['jtitle'])
-        print(request.form['ep'])
-        print('RUNNING 3.0')
+        # Uses sent data from Chrome Extension to update user's anime entry on MAL
         updateMAL(title, jtitle, ep)
     return 'OK'
 
+
+# Updates the anime on MAL to the most recently watched ep on anime website
 def updateMAL(title, jtitle, ep):
-  r = requests.get('https://api.myanimelist.net/v2/anime/suggestions?limit=4')
-  print(r.status_code)
+  print(title + " | " + jtitle)
+  # Searches for the ID of the anime user is currently watching
+  id = findAnimeID(title, jtitle)
+  if id == None:
+    print(title + " | " + jtitle + " : CANNOT BE FOUND IN USERS MAL LIST" )
+  else:
+    print("Works")
+
+
+# Searches the users current anime list on MAL and returns the specific anime id value,
+# Returns None if anime entry does not exist on MAL
+def findAnimeID(title, jtitle):
+  r = requests.get('https://api.myanimelist.net/v2/users/@me/animelist?fields=list_status&limit=1000', headers=key).json()
+  r = r['data']
+  # Loops through user's MAL list in search for anime user is currently watching
+  for anime in r:
+    name = anime['node']['title']
+    if name.lower() == title.lower() or name.lower() == jtitle.lower():
+      print(anime)
+      return anime['node']['id']
 
 
 if __name__ == '__main__':
